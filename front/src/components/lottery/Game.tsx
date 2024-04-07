63a6ee07048399635dce7beb844c887b750bfac9
lsy969999 <lsy969999@gmail.com>
2024-04-08 04:21:27 +0900

    머신 파편화 및 동적로딩

--- front/src/components/lottery/Game.tsx
+++ front/src/components/lottery/Game.tsx
@@ -1,12 +1,11 @@
-import { Text, useTexture } from "@react-three/drei"
-import { CapsuleCollider, Physics, RapierRigidBody, RigidBody } from "@react-three/rapier";
-import { useEffect, useMemo, useRef, useState } from "react";
+import {  useTexture } from "@react-three/drei"
+import {  Physics, RapierRigidBody, RigidBody } from "@react-three/rapier";
+import {  useMemo, useRef } from "react";
 import { ballInjectorPosRef, balls } from "./const";
 import { Machine } from "./Machine";
 import { getRandomBetween } from "../../utils/math";
 import { button, useControls } from "leva";
 const Game = () => {
-    
     const randomBalls = useMemo(()=>{
         const rBalls = ballRandomPosGenerate();
         return rBalls;
@@ -17,17 +16,11 @@ const Game = () => {
         ballFixed: false,
         reset: button(() => {
             console.log('todo')
+        }),
+        start: button(()=>{
+            console.log('start');
         })
     })
-
-    const [tmp, setTmp] = useState(false);
-    useEffect(()=>{
-        setTimeout(()=>{
-            console.log(tmp)
-            setTmp(true)
-            console.log(tmp)
-        }, 5000)
-    }, [tmp])
     return (
         <Physics debug={ rapierDebug } timeStep={1/300} maxCcdSubsteps={1}>
             <directionalLight
@@ -49,31 +42,6 @@ const Game = () => {
                         />
                     )
                 })
-            }
-
-            {
-                tmp &&
-                <RigidBody
-                    type="fixed"
-                    position={ [ 0, -0.55, 0 ] }
-                >
-                    <CapsuleCollider
-                        args={ [ 0.4, 0.3 ] }
-                        sensor
-                        onIntersectionEnter={(payload) => {
-                            // console.log("Goal!", payload)
-                            
-                            // payload.target.rigidBody?.applyImpulse({ x: 0, y: 0.01, z: 0 }, true)
-                            
-                                payload.other.rigidBody?.applyImpulse({ x: 0, y: 0.06, z: 0 }, true)
-                                // console.log('isCcdEnabled', payload.other.rigidBody?.isCcdEnabled())
-                                payload.other.rigidBody?.enableCcd(true)
-                                if(!payload.other.rigidBody?.isCcdEnabled()){
-                                    payload.other.rigidBody?.enableCcd(true)
-                                }
-                        }}
-                    />
-                </RigidBody>
             }
 
             {/* <Text
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,11 +1,23 @@
-import Lottery from "../components/lottery/Lottery"
+import { Suspense, lazy } from "react"
+import { balls } from "../components/lottery/const"
+import { useTexture } from "@react-three/drei"
+
+const LotteryLazy = lazy(() => import('../components/lottery/Lottery'))
 
 const Home = () => {
   return (
     <div>
-        <Lottery/>
+        <Suspense fallback={<div>loading...</div>}>
+            <LotteryLazy/>
+        </Suspense>
     </div>
   )
 }
 
 export default Home
+
+
+
+balls.forEach(item=>{
+    useTexture.preload(item.texture);
+})
